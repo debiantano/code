f59aba05a53e7d760b7e0728e713cabb875182e6
debiantano <68764921+debiantano@users.noreply.github.com>
2021-01-13 20:25:01 -0500

    Create numero_aleatorio.cpp

---
+++ c++/numero_aleatorio.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int num, c;
+    srand(time(NULL));
+    
+ 	cout<<"NUMERO ALEATORIO ENTRE 1 y 10"<<endl;
+    num =1+rand()%(100);
+    cout<<"El numeros es: "<<num<<endl;
+    
+    return 0;
+}
