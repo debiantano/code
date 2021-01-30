f0d67c516fa7b903354fc72d816c29e9ae0138f7
debiantano <68764921+debiantano@users.noreply.github.com>
2021-01-30 17:36:37 -0500

    Update interando_path.sh

--- bash/interando_path.sh
+++ bash/interando_path.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 my_path=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/usr/local/go/bin:/home/noroot/.local/bin
+echo -e "\nITERANDO EL PATH"
 for path in $(echo $my_path | tr ":" " "); do
     echo $path 
 done
+echo -e "\nCAMBIAR CARACTERES"
+echo "cconvirtiendo los espacion en mas" | sed 's/ /+/g'
+echo "cconvirtiendo los espacion en mas" | tr ' ' '+'
