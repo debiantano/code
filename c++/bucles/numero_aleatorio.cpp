285c3a1c346755a3c7ac0e25340bc6aadd88c39c
debiantano <68764921+debiantano@users.noreply.github.com>
2021-01-31 23:55:54 -0500

    Update numero_aleatorio.cpp

--- c++/bucles/numero_aleatorio.cpp
+++ c++/bucles/numero_aleatorio.cpp
@@ -8,7 +8,7 @@ int main()
     int num, c;
     srand(time(NULL));
     
- 	  cout<<"NUMERO ALEATORIO ENTRE 1 y 10"<<endl;
+    cout<<"NUMERO ALEATORIO ENTRE 1 y 10"<<endl;
     num =1+rand()%(10);
     cout<<"El numeros es: "<<num<<endl;
     
