1021cec6ab7a4f6685164ca9f2df32f54361501b
debiantano <68764921+debiantano@users.noreply.github.com>
2021-03-05 19:00:14 -0900

    Create datos_no_primitivos.java

---
+++ java/datos_no_primitivos.java
@@ -0,0 +1,19 @@
+package holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        // Las variables NO primitivos almacenan NULL, ademas de poseer metodos
+        // Integer String
+        Integer numero=10; 
+        Integer num2=null;
+        String palabra="Soy una cadena";
+        String palabra2=null;
+        
+        System.out.println(numero);
+        System.out.println(num2);
+        System.out.println(palabra);
+        System.out.println(palabra2);
+    }
+    
+}
