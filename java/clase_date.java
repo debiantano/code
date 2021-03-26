c333110737225e88503c9c5c3592a9165eda1f7d
debiantano <68764921+debiantano@users.noreply.github.com>
2021-03-26 11:28:49 -0900

    Update clase_date.java

--- java/clase_date.java
+++ java/clase_date.java
@@ -10,9 +10,12 @@ public class Temporales {
         
         SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat sdf2=new SimpleDateFormat("dd-MM-yyyy");
+        
         String fechaFormateada=sdf.format(fechaActual);
+        String fechaFormateada2=sdf2.format(fechaActual);
         
         System.out.println(fechaFormateada);
+        System.out.println(fechaFormateada2);
         System.out.println();
     }  
 }
