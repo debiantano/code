342b48c79b318a6c0c809ebcc238f6690c4fc927
debiantano <68764921+debiantano@users.noreply.github.com>
2021-03-28 19:50:17 -0900

    Create excepcion_verificada.java

---
+++ java/excepciones/excepcion_verificada.java
@@ -0,0 +1,22 @@
+
+package temporales;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class errores {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        // EXCEPCIONES VERIFICADAS (IOException)
+        // LECTURA DE UN ARCHIVO DE TEXTO
+         BufferedReader bf = new BufferedReader(new FileReader("d:\\test.txt"));
+         String cadena;
+         
+         while((cadena = bf.readLine()) != null){
+             System.out.println(cadena);
+         }
+         
+    }
+}
