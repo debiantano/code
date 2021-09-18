208277e7677fc3ef7fc026b5efe9180674026df2
debiantano <cromozzc@gmail.com>
2021-09-18 11:52:34 -0500

    c1

---
+++ machines_ctf/xss_mysql_VH/index.php
@@ -0,0 +1,23 @@
+<?php
+  $site = "PentesterLab vulnerable blog";
+  require "header.php";
+  $posts = Post::all();
+?>
+  <div class="block" id="block-text">
+    <div class="secondary-navigation">
+      <div class="content">
+      <?php 
+        foreach ($posts as $post) {
+            echo $post->render(); 
+      } ?> 
+     </div>
+ 
+    </div>
+  </div>
+
+
+<?php
+
+
+  require "footer.php";
+?>
