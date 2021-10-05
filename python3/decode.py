aa74c6f31bb74fbccb3549018b64c9e54e57e65b
NULL <68764921+debiantano@users.noreply.github.com>
2021-10-05 00:05:02 -0500

    Update decode.py

--- python3/decode.py
+++ python3/decode.py
@@ -3,7 +3,9 @@
 import sys, os
 from urllib.parse import unquote
 
-decode = unquote(sys.argv[1]);
-
-print(decode)
-os.system("echo '{}' | xclip -sel clip".format(decode))
+if len(sys.argv) != 2:
+    print("Use: {} <str>".format(sys.argv[0]))
+else:
+    decode = unquote(sys.argv[1]);
+    print(decode)
+    os.system("echo '{}' | xclip -sel clip".format(decode))
