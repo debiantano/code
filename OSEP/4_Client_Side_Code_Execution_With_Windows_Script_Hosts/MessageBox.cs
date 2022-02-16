761639aa331cdf10391a25cb5e3e0a93c51fd0d2
NULL <68764921+debiantano@users.noreply.github.com>
2022-02-16 11:01:18 -0500

    Create MessageBox.cs

---
+++ OSEP/4_Client_Side_Code_Execution_With_Windows_Script_Hosts/MessageBox.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+[ComVisible(true)]
+public class TestClass
+{
+    public TestClass()
+    {
+        MessageBox.Show("Test", "Test", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+    }
+
+    public void RunProcess(string path)
+    {
+        Process.Start(path);
+    }
+}
+
