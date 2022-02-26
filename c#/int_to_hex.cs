c8c61fc4c12b107439e8fc1329d8488154aac75e
NULL <68764921+debiantano@users.noreply.github.com>
2022-02-25 20:24:43 -0500

    Create int_to_hex.cs

---
+++ c#/int_to_hex.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace file_size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 99;
+            string hex = i.ToString("X");
+            Console.WriteLine(hex);
+        }
+    }
+}
