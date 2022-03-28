6f69deb47c65426e91aba4e03874a3c8414611a4
NULL <68764921+debiantano@users.noreply.github.com>
2022-03-28 17:01:40 -0500

    Update and rename poc_smtp.py to poc_smtplib.py

--- OSWE/2_Atmail_Mail_Server_Appliance:_from_XSS_to_RCE/poc_smtplib.py
+++ OSWE/2_Atmail_Mail_Server_Appliance:_from_XSS_to_RCE/poc_smtplib.py
@@ -2,7 +2,7 @@ import smtplib
 
 sender_email = "cromozzc@gmail.com"
 rec_email = "richar.quispe@gmail.com"
-password = input(str("> "))
+password = input(str("> ")) 
 message = "Hola, este es un mensaje usando python"
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
