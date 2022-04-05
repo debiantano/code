add4b006844e37765808442226bbdbec2b189e9b
NULL <68764921+debiantano@users.noreply.github.com>
2022-04-05 10:52:45 -0500

    Create args.cs

---
+++ c#/args.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argumento: " + args[0]);
+        }
+    }
+}
