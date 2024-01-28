9574ab48d7415f2f54f1eec4473e536a0fa297b8
NULL <68764921+debiantano@users.noreply.github.com>
2024-01-27 19:29:46 -0500

    Delete configuraciones/poly-bspwm-sxhkd directory

--- configuraciones/poly-bspwm-sxhkd/ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-target=$(ifconfig | grep eth0 -A 1 | awk '{print $NF}' | awk NR==2)
-
-if [ $target ]; then
-    echo "%{F#e51d0b}%{F#ffffff} $target%{u-}"
-else
-    echo "%{F#e51d0b} %{u-}%{F#ffffff} No target"
-fi
